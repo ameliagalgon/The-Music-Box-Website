bf9a0b24dc7809bd8eb19adef7ca3155bca65fdf
ameliagalgon <amg617@lehigh.edu>
2016-07-07 16:18:01 -0400

    attempting to resolve gallery masonry grid

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    alert("hello");
+    
+    uploadImages().done(function(){
+        //initalize grid
+        $('.grid').masonry({
+            // options
+            itemSelector: '.grid-item',
+            columnWidth: 200
+        });
+    });
 });
+
+function uploadImages(){
+    var x = $.Deferred();
+    for(var i=0; i< 19; i++){
+        /** upload the 19 photos from the gallery folder **/
+        var img = new Image();
+        img.src = "img/gallery/img-" + i + ".jpg";
+        var item = $("<div class='grid-item'></div>")
+        item.append(img);
+        $("#gallery .grid").append(item);
+    }
+    x.resolve();
+}
